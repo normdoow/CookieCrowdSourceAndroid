d1a890ba02c81b6a3042d3c4c3900310570c5592
noahbragg <noahbragg@cedarville.edu>
2017-10-01 10:16:45 -0400

    added the start to the rest api with retrofit

--- CookieCrowdSource/app/src/main/java/shinzzerz/cookiecrowdsource/CookieCrowdSourceMainActivity.java
+++ CookieCrowdSource/app/src/main/java/shinzzerz/cookiecrowdsource/CookieCrowdSourceMainActivity.java
@@ -5,11 +5,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import java.io.IOException;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import shinzzerz.restapi.CookieAPI;
+
 /**
  * Created by administratorz on 9/2/2017.
  */
 
 public class CookieCrowdSourceMainActivity extends AppCompatActivity{
+
+    CookieAPI cookieAPI;
 
     @Override
     public void onCreate(Bundle savedInstance){
@@ -19,5 +30,54 @@ public class CookieCrowdSourceMainActivity extends AppCompatActivity{
 
         Intent locationIntent = new Intent(this, CookieCrowdSourceLocationActivity.class);
         startActivity(locationIntent);
+
+        //init the Retrofit instance for rest api
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(CookieAPI.BASE_URL)
+                .build();
+
+        cookieAPI = retrofit.create(CookieAPI.class);
+
+        apiCalls();
+    }
+
+    private void apiCalls() {
+        Call<ResponseBody> callCookAvailable = cookieAPI.isCookAvailable();
+        callCookAvailable.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                boolean isCookAvailable = false;
+                try {
+                    isCookAvailable = response.body().string().equals("True");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                System.out.println();
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                System.out.println();
+            }
+        });
+
+        Call<ResponseBody> callCreateCustomer = cookieAPI.createCustomer();
+        callCreateCustomer.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                String customerId = "";
+                try {
+                    customerId = response.body().string();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                System.out.println();
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                System.out.println();
+            }
+        });
     }
 }
