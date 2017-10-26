e857b264baa3d109084b5467a00e1bd9c4e9ea84
noahbragg <noahbragg@cedarville.edu>
2017-10-25 22:33:45 -0400

    made the location and cookie status update every 20 seconds

--- CookieCrowdSource/app/src/main/java/shinzzerz/cookiecrowdsource/CookieCrowdSourceMainActivity.java
+++ CookieCrowdSource/app/src/main/java/shinzzerz/cookiecrowdsource/CookieCrowdSourceMainActivity.java
@@ -5,9 +5,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.CountDownTimer;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -21,6 +19,7 @@ import com.stripe.wrap.pay.AndroidPayConfiguration;
 import com.stripe.wrap.pay.utils.CartManager;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -30,6 +29,9 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
+import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
 import shinzzerz.io.CookieIO;
 import shinzzerz.location.DistTypeEnum;
 import shinzzerz.location.GetCurrentLocation;
@@ -86,6 +88,18 @@ public class CookieCrowdSourceMainActivity extends AppCompatActivity {
         cookieAPI = retrofit.create(CookieAPI.class);
 
         apiCalls();
+
+        //run async task that checks for location and api calls every 20 seconds to udpate the view
+        Observable.interval(20, TimeUnit.SECONDS)
+            .subscribeOn(AndroidSchedulers.mainThread())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(new Action1<Long>() {
+                @Override
+                public void call(Long aLong) {
+                    apiCalls();
+                    setupLocation();
+                }
+            });
     }
 
     @Override
@@ -201,149 +215,100 @@ public class CookieCrowdSourceMainActivity extends AppCompatActivity {
         else if(!obtainedLocation) {
             SimpleDistance mySimpleDistance = new SimpleDistance();
             myLocation.getDistanceInMeters(this, new SimpleLocation(CDC_LAT, CDC_LONG), mySimpleDistance);
-            (new onLocationAcquired()).execute(mySimpleDistance, this);
+
+            updateButtonBasedOnCookieLogic(mySimpleDistance);
         }
     }
 
-    private class onLocationAcquired extends AsyncTask<Object, Void, Void> {
-        private SimpleDistance dist;
-        private onLocationAcquired onALocationAcquired;
-        private CookieCrowdSourceMainActivity cookieCrowdSourceMainActivity;
+    private void updateButtonBasedOnCookieLogic(SimpleDistance dist) {
+        int distance = (int) Math.ceil(dist.getDistance(DistTypeEnum.Miles));
 
-        //This preExecute will start a timer to shut off this onLocationAcquired
-        @Override
-        protected void onPreExecute() {
-            onALocationAcquired = this;
-
-            new CountDownTimer(timeBeforeLocationTimeout, 7000) {
-                public void onTick(long millisUntilFinished) {
-                    // You can monitor the progress here as well by changing the onTick() time
-                }
-                public void onFinish() {
-                    // stop async task if not in progress
-                    if (onALocationAcquired.getStatus() == Status.RUNNING) {
-                        onALocationAcquired.cancel(true);
-                        // Add any specific task you wish to do as your extended class variable works here as well.
-                    }
-                }
-            }.start();
+        //Do cookie logic!
+        if(distance >= 400 && !cookiesAvailable){
+            outsideLocationAndNoCookies();
         }
-
-        @Override
-        protected Void doInBackground(Object... params) {
-            dist = (SimpleDistance)params[0];
-            cookieCrowdSourceMainActivity = (CookieCrowdSourceMainActivity)params[1];
-
-            //spinlock this thread
-            while (!myLocation.isLocationInitialized() && !isCancelled()) {
-                //wait
-            }
-
-            if(isCancelled()){//This will only be true if a timeout occurred after 30 seconds on location
-                outsideLocation();
-                myLocation.stopLoadingLocation();
-            }
-
-            return null; //Return is necessary to explicitly notify that the doInBackground is done.
+        else if(distance >= 400 && cookiesAvailable){
+            outsideLocation();
+        }else if(distance <= 359 && !cookiesAvailable){
+            noCookies();
         }
-
-        @Override
-        protected void onPostExecute(Void params) {
-            obtainedLocation = true;
-            int distance = (int) Math.ceil(dist.getDistance(DistTypeEnum.Miles));
-//            myButton.setText("Distance: " + distance);
-
-            apiCalls();
-
-
-            //Do cookie logic!
-            if(distance >= 400 && !cookiesAvailable){
-                outsideLocationAndNoCookies();
-            }
-            else if(distance >= 400 && cookiesAvailable){
-                outsideLocation();
-            }else if(distance <= 359 && !cookiesAvailable){
-                noCookies();
-            }
-            else{
-                haveCookies();
-            }
-
+        else{
+            haveCookies();
         }
+    }
 
-        private void outsideLocationAndNoCookies(){
-            getCookiesButton.setText("Why can't I get cookies?");
-            getCookiesButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    AlertDialog alertDialog = new AlertDialog.Builder(context).create();
-                    alertDialog.setTitle("Wrong Location and No Cookies");
-                    alertDialog.setMessage("There are no cooks that are making cookies currently. Try again in the evening from 5pm to 9pm. There is more chance that we will be making cookies then! You also must be in a location that is in a 5 mile radius around the Greene to be able to order cookies. Thank you for your patience while we are getting this new business idea up and running!");
-                    alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "Ok",
-                            new DialogInterface.OnClickListener() {
-                                public void onClick(DialogInterface dialog, int which) {
-                                    dialog.dismiss();
-                                }
-                            });
-                    alertDialog.show();
-                }
-            });
-        }
-
-        private void outsideLocation(){
-            getCookiesButton.setText("Why can't I get cookies?");
-            getCookiesButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    AlertDialog alertDialog = new AlertDialog.Builder(context).create();
-                    alertDialog.setTitle("Wrong Location");
-                    alertDialog.setMessage("You must be in a location that is in a 5 mile radius from the Greene for you to be able to order cookies. We will hopefully be coming to a location closer to you soon! Thank you for your patience while we are getting this new business idea up and running!");
-                    alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "Ok",
-                            new DialogInterface.OnClickListener() {
-                                public void onClick(DialogInterface dialog, int which) {
+    private void outsideLocationAndNoCookies(){
+        getCookiesButton.setText("Why can't I get cookies?");
+        getCookiesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog alertDialog = new AlertDialog.Builder(context).create();
+                alertDialog.setTitle("Wrong Location and No Cookies");
+                alertDialog.setMessage("There are no cooks that are making cookies currently. Try again in the evening from 5pm to 9pm. There is more chance that we will be making cookies then! You also must be in a location that is in a 5 mile radius around the Greene to be able to order cookies. Thank you for your patience while we are getting this new business idea up and running!");
+                alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "Ok",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
                                 dialog.dismiss();
-                                }
-                            });
-                    alertDialog.show();
-                }
-            });
-        }
+                            }
+                        });
+                alertDialog.show();
+            }
+        });
+    }
 
-        private void noCookies(){
-            getCookiesButton.setText("Why can't I get cookies?");
-            getCookiesButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    AlertDialog alertDialog = new AlertDialog.Builder(context).create();
-                    alertDialog.setTitle("No Cookies");
-                    alertDialog.setMessage("There are no cooks that are making cookies currently. Try again in the evening from 5pm to 9pm. There is more chance that we will be making cookies then! Thank you for your patience while we are getting this new business idea up and running!");
-                    alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "Ok",
-                            new DialogInterface.OnClickListener() {
-                                public void onClick(DialogInterface dialog, int which) {
-                                    dialog.dismiss();
-                                }
-                            });
-                    alertDialog.show();
-                }
-            });
-        }
+    private void outsideLocation(){
+        getCookiesButton.setText("Why can't I get cookies?");
+        getCookiesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog alertDialog = new AlertDialog.Builder(context).create();
+                alertDialog.setTitle("Wrong Location");
+                alertDialog.setMessage("You must be in a location that is in a 5 mile radius from the Greene for you to be able to order cookies. We will hopefully be coming to a location closer to you soon! Thank you for your patience while we are getting this new business idea up and running!");
+                alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "Ok",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+                alertDialog.show();
+            }
+        });
+    }
 
-        private void haveCookies(){
-            getCookiesButton.setText("Get Cookies!");
-            getCookiesButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    CartManager cartManager = new CartManager();
-                    cartManager.addLineItem(StoreUtils.getEmojiByUnicode(0x1F36A), (double) 1, 1000);
-                    try {
-                        Cart cart = cartManager.buildCart();
-                        Intent paymentLaunchIntent = PaymentActivity.createIntent(cookieCrowdSourceMainActivity, cart);
-                        startActivity(paymentLaunchIntent);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
+    private void noCookies(){
+        getCookiesButton.setText("Why can't I get cookies?");
+        getCookiesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog alertDialog = new AlertDialog.Builder(context).create();
+                alertDialog.setTitle("No Cookies");
+                alertDialog.setMessage("There are no cooks that are making cookies currently. Try again in the evening from 5pm to 9pm. There is more chance that we will be making cookies then! Thank you for your patience while we are getting this new business idea up and running!");
+                alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "Ok",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+                alertDialog.show();
+            }
+        });
+    }
+
+    private void haveCookies(){
+        getCookiesButton.setText("Get Cookies!");
+        getCookiesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CartManager cartManager = new CartManager();
+                cartManager.addLineItem(StoreUtils.getEmojiByUnicode(0x1F36A), (double) 1, 1000);
+                try {
+                    Cart cart = cartManager.buildCart();
+                    Intent paymentLaunchIntent = PaymentActivity.createIntent(context, cart);
+                    startActivity(paymentLaunchIntent);
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
-            });
-        }
+            }
+        });
     }
 }
