e58122d78a8b91c5d64a1806c4735b3011d3967c
noahbragg <noahbragg@cedarville.edu>
2017-10-28 18:05:37 -0400

    ready for production

--- CookieCrowdSource/app/src/main/java/shinzzerz/Key.kt
+++ CookieCrowdSource/app/src/main/java/shinzzerz/Key.kt
@@ -4,6 +4,6 @@ package shinzzerz
  * Created by noahbragg on 10/23/17.
  */
 
-val STRIPE_PUBLISHABLE_KEY = "pk_test_tAMChOZmT4OHrVNyhGvJmuLH"
+val STRIPE_PUBLISHABLE_KEY = "pk_live_9Kcag7bFv5h1BM96L9FBDWxa"
 
 val PUSHER_APP_KEY = "d05669f4df7a1f96f929"
