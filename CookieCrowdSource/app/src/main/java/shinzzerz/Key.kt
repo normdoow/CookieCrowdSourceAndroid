71fc4454d36766e6ce6c89c86f09cac619dce673
noahbragg <noahbragg@cedarville.edu>
2017-11-26 14:04:15 -0500

    added key

--- CookieCrowdSource/app/src/main/java/shinzzerz/Key.kt
+++ CookieCrowdSource/app/src/main/java/shinzzerz/Key.kt
@@ -5,5 +5,6 @@ package shinzzerz
  */
 
 val STRIPE_PUBLISHABLE_KEY = "pk_live_9Kcag7bFv5h1BM96L9FBDWxa"
+//val STRIPE_PUBLISHABLE_KEY = "pk_test_tAMChOZmT4OHrVNyhGvJmuLH"
 
 val PUSHER_APP_KEY = "d05669f4df7a1f96f929"
